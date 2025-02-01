ae7273ddef317dea70b1a8bc3c0e7f6730ca61b6
Betsabe Iglesias Marques <beiglesi@c4r5s6.42urduliz.com>
2025-02-01 15:13:53 +0100

    inc src created

---
+++ inc/minirt.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beiglesi <beiglesi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 14:24:31 by beiglesi          #+#    #+#             */
+/*   Updated: 2025/02/01 14:30:26 by beiglesi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+#define MINIRT_H
+
+# include "variables.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include <fcntl.h>
+
+
+#endif
---
+++ inc/variables.h
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   variables.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beiglesi <beiglesi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/01 14:30:53 by beiglesi          #+#    #+#             */
+/*   Updated: 2025/02/01 14:47:30 by beiglesi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+typedef struct s_ambient
+{
+	float	ratio;
+	int		color;
+}			t_ambient;
+
+typedef struct s_position
+{
+	float	x;
+	float	y;
+	float	z;
+}			t_position;
+
+typedef struct s_vector
+{
+	float	x;
+	float	y;
+	float	z;
+}			t_vector;
+
+typedef struct s_camera
+{
+	t_position	pos;
+	t_vector	vec;
+	int			fov;
+
+}			t_camera;
+
+
+typedef struct s_light
+{
+	t_position	pos;
+	float		bright;
+	int			color;
+}				t_light;
+
+typedef	enum e_eltype
+{
+	SP,
+	CY,
+	PL
+}			t_eltype;
+
+typedef struct s_element
+{
+	t_eltype	type;
+	t_position	pos;
+	int			color;
+	float		diam;
+	float		height;
+	t_vector	vec;
+	
+}			t_element;
+
+typedef struct s_data
+{
+	t_ambient	amb;
+	t_camera	cam;
+	t_light		lig;
+	t_element	*elem;
+}				t_data;
